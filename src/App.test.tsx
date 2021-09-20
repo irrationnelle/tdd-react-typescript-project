8e6192e38a5544b13e822305a00f32ffaece1c31
irrationnelle <drakkarverenis@gmail.com>
2021-09-21 01:33:16 +0900

    test: Remove redundant test for sample code

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,12 +2,6 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
 test('renders input tag for id', () => {
   render(<App />);
   const idInput = screen.getByLabelText('id-input');
