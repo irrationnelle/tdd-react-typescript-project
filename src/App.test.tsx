ff09a945da81883a0a67408f7330ed61448b044f
irrationnelle <drakkarverenis@gmail.com>
2021-09-21 01:27:20 +0900

    test: Add test for id input tag

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -7,3 +7,9 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders input tag for id', () => {
+  render(<App />);
+  const idInput = screen.getByLabelText('id-input');
+  expect(idInput).toBeInTheDocument();
+})
