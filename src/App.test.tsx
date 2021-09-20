c14f1a82217bc7fbd22ee2caaa06b5da5aacefb9
irrationnelle <drakkarverenis@gmail.com>
2021-09-21 01:37:08 +0900

    test: Extract render function from each test for refactoring

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,20 +2,23 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders input tag for id', () => {
-  render(<App />);
-  const idInput = screen.getByLabelText('id-input');
-  expect(idInput).toBeInTheDocument();
-})
+describe('App component', () => {
+  beforeEach(() => {
+    render(<App />);
+  })
 
-test('renders input tag for password', () => {
-  render(<App />);
-  const idInput = screen.getByLabelText('password-input');
-  expect(idInput).toBeInTheDocument();
-})
+  test('renders input tag for id', () => {
+    const idInput = screen.getByLabelText('id-input');
+    expect(idInput).toBeInTheDocument();
+  })
 
-test('renders button tag to submit', () => {
-  render(<App />);
-  const submitButton = screen.getByRole('button');
-  expect(submitButton).toBeInTheDocument();
+  test('renders input tag for password', () => {
+    const idInput = screen.getByLabelText('password-input');
+    expect(idInput).toBeInTheDocument();
+  })
+
+  test('renders button tag to submit', () => {
+    const submitButton = screen.getByRole('button');
+    expect(submitButton).toBeInTheDocument();
+  })
 })
