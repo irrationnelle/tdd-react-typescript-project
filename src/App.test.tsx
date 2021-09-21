bb7d2bddc54ae3431478ccc903b4f0c85c37e579
irrationnelle <drakkarverenis@gmail.com>
2021-09-22 00:24:27 +0900

    test: Add test to check submit button

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
 
-describe('App component', () => {
+describe("App component", () => {
   beforeEach(() => {
     render(<App />);
-  })
+  });
 
-  test('renders input tag for id', () => {
-    const idInput = screen.getByLabelText('id-input');
+  test("renders input tag for id", () => {
+    const idInput = screen.getByLabelText("id-input");
     expect(idInput).toBeInTheDocument();
-  })
+  });
 
-  test('renders input tag for password', () => {
-    const idInput = screen.getByLabelText('password-input');
+  test("renders input tag for password", () => {
+    const idInput = screen.getByLabelText("password-input");
     expect(idInput).toBeInTheDocument();
-  })
+  });
 
-  test('renders button tag to submit', () => {
-    const submitButton = screen.getByRole('button');
+  test("renders button tag to submit", () => {
+    const submitButton = screen.getByRole("button");
     expect(submitButton).toBeInTheDocument();
-  })
-})
+  });
+
+  test("renders main page after clicking submit button", () => {
+    const submitButton = screen.getByRole("button");
+    fireEvent.click(submitButton);
+    const mainPageElement = screen.getByText(/main page/);
+    expect(mainPageElement).toBeInTheDocument();
+  });
+});
