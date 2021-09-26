94f65259d1329fd267c2acbcdd370cd1116b9bf1
root <root@vultr.guest>
2021-09-26 08:46:50 +0000

    test: Add test to implement typing id and password

--- src/Login.test.tsx
+++ src/Login.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Login from "./Login";
 
 describe("Login component", () => {
@@ -20,5 +20,18 @@ describe("Login component", () => {
   test("renders button tag to submit", () => {
     const submitButton = screen.getByRole("button");
     expect(submitButton).toBeInTheDocument();
+  });
+
+  test("should input id and password", () => {
+    const idInput = screen.getByLabelText("id-input");
+    fireEvent.change(idInput, { target: { value: "exampleid" } });
+
+    const passwordInput = screen.getByLabelText("password-input");
+    fireEvent.change(passwordInput, { target: { value: "password123" } });
+
+    const idText = screen.getByDisplayValue("exampleId");
+    expect(idText).toHaveAttribute("aria-label", "id-input");
+    const passwordText = screen.getByDisplayValue("password123");
+    expect(passwordText).toBeInTheDocument();
   });
 });
