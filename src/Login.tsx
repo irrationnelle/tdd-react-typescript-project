7a801ff54ddf1e02cde3de77d5e545e9204c5b7e
root <root@vultr.guest>
2021-09-26 10:06:22 +0000

    feat: Add useState hook to control id and password state

--- src/Login.tsx
+++ src/Login.tsx
@@ -5,11 +5,25 @@ interface LoginProps {
 }
 
 const Login: React.FC<LoginProps> = ({ isSignIn }: LoginProps) => {
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <div className="App">
-      <input aria-label="id-input" />
-      <input aria-label="password-input" />
-      <button onClick={isSignIn} disabled>
+      <input
+        aria-label="id-input"
+        value={id}
+        onChange={(event) => {
+          setId(event.target.value);
+        }}
+      />
+      <input
+        aria-label="password-input"
+        value={password}
+        onChange={(event) => {
+          setPassword(event.target.value);
+        }}
+      />
+      <button onClick={isSignIn} disabled={!id || !password}>
         submit
       </button>
     </div>
