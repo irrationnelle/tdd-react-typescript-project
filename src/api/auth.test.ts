1db64b30033df885f2d93833d367796349b25581
irrationnelle <drakkarverenis@gmail.com>
2021-10-03 12:49:22 +0000

    debug: Debug firebase unit test

--- src/api/auth.test.ts
+++ src/api/auth.test.ts
@@ -1,4 +1,32 @@
 import { signIn } from "./auth";
+import { firebaseConfig } from "../constant/firebase-config";
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  connectAuthEmulator,
+  UserCredential,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+connectAuthEmulator(auth, "http://localhost:9099");
+
+test("Example test case", async () => {
+  try {
+    const cred: UserCredential = await signInWithEmailAndPassword(
+      auth,
+      "foo@bar.de",
+      "bla2377"
+    );
+    expect(cred).toBeTruthy();
+    expect(cred.user).toBeTruthy();
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+});
 
 test("signIn function should communicate with api server to recognize valid account", () => {
   const id = "valid_id";
---
+++ src/constant/firebase-config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDyMcJQD4uPv6wzjeZa0U7UnyYIW4Kje5Q",
+  authDomain: "tdd-typescript-react.firebaseapp.com",
+  projectId: "tdd-typescript-react",
+  storageBucket: "tdd-typescript-react.appspot.com",
+  messagingSenderId: "791594135348",
+  appId: "1:791594135348:web:eeac5926f784ff076c7f61",
+  measurementId: "G-0Z03KDDJ01",
+};
